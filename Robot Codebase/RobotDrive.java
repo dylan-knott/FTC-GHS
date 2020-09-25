c138360aa84edc2b46383cd903ef11bdb7cc2b5b
Dylan Knott <dgknott33@gmail.com>
2020-09-25 01:32:33 -0500

    Added libOpenCV file to add to robot controller phone for dogeCV to work

--- Robot Codebase/RobotDrive.java
+++ Robot Codebase/RobotDrive.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.ftc17223;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.*;
--- Robot Codebase/VuforiaClass.java
+++ Robot Codebase/VuforiaClass.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.ftc17223;
+package org.firstinspires.ftc.teamcode;
 
 
 import android.graphics.Camera;
